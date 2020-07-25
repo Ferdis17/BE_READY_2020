c8263e21106b77a63aabae167365dbac8d887e46
Ferdis17 <fmuriyesu@gmail.com>
2020-07-24 21:12:37 -0400

    first commit

---
+++ coding_interviews/src/bigO/PairSumSequence.java
@@ -0,0 +1,23 @@
+package bigO;
+
+public class PairSumSequence {
+
+    public static int pairSumSequence(int n){
+        int sum = 0;
+
+        for (int i = 0; i < n; i++){
+            sum = pairSum(i , i+1);
+        }
+        return sum;
+    }
+
+    private static int pairSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(pairSumSequence(5));
+
+    }
+}
