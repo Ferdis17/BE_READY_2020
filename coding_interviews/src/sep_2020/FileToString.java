bfc07c8b2a09c025680aaaa854865d95f6f8959d
Ferdis17 <fmuriyesu@gmail.com>
2020-09-09 11:26:18 -0400

    read the text from file

---
+++ coding_interviews/src/sep_2020/FileToString.java
@@ -0,0 +1,16 @@
+package sep_2020;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+public class FileToString {
+
+    public static void main(String[] args) throws IOException {
+        //with java 8 it's amazing, just one line.
+        String content = Files.lines(Paths.get("/Users/Ferdis/Documents/greeting.txt"), StandardCharsets.UTF_8).collect(Collectors.joining());
+        System.out.print(content);
+    }
+}
