1cf76b6e550db035801e356b693d77a514c13604
Ferdis17 <fmuriyesu@gmail.com>
2020-09-16 11:38:01 -0400

    if int, the use hard frute force, don't do that

---
+++ coding_interviews/src/sep_2020/RepeatedString.java
@@ -0,0 +1,40 @@
+package sep_2020;
+
+public class RepeatedString {
+
+    /**
+     * don't do this plz
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println(repeatedString("aba", 10));
+    }
+    public static long repeatedString(String s, int n) {
+        if(null == s || s.length()<1){
+            return 0;
+        }
+        if (n == 0) {
+            return 0;
+        }
+        int count = 0;
+        String newS = null;
+        if(n < s.length()){
+            newS = s.substring(n);
+        }
+        if (n > s.length()){
+            if(n % s.length() == 0){
+                newS = s.repeat(n/s.length());
+            } else {
+                int mod = n % s.length();
+                newS = s.repeat((n-mod)/s.length()) + s.substring(mod);
+            }
+        }
+        for(int i = 0; i < newS.length(); i++){
+            if(newS.charAt(i) == 'a'){
+                count++;
+            }
+        }
+        return count;
+    }
+}
