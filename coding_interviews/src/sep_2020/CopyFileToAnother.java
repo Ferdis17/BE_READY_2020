447afee40224c4596fe4c7b725d7c2d42ba80b74
Ferdis17 <fmuriyesu@gmail.com>
2020-09-21 20:00:27 -0400

    count substring frequency and copy file to another

---
+++ coding_interviews/src/sep_2020/CopyFileToAnother.java
@@ -0,0 +1,17 @@
+package sep_2020;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyFileToAnother {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream origin = new FileInputStream("/Users/Ferdis/Documents/greeting.txt");
+        FileOutputStream destination = new FileOutputStream("/Users/Ferdis/Desktop/copyFile.txt");
+
+        int b;
+        while ((b = origin.read()) != -1)
+            destination.write(b);
+    }
+}
---
+++ coding_interviews/src/sep_2020/forEb/CountSubString.java
@@ -0,0 +1,37 @@
+package sep_2020.forEb;
+
+public class CountSubString {
+
+    static int countFreq(String subString, String str) {
+        int M = subString.length();
+        int N = str.length();
+        int count = 0;
+
+        /* A loop to slide subString[] one by one */
+        for (int i = 0; i <= N - M; i++) {
+            /* For current index i, check for
+        pattern match */
+          int j;
+            for (j = 0; j < M; j++) {
+                if (str.charAt(i + j) != subString.charAt(j)) {
+                    break;
+                }
+            }
+
+            // if subString[0...M-1] = str[i, i+1, ...i+M-1]
+            if (j == M) {
+                count++;
+                j = 0;
+            }
+        }
+        return count;
+    }
+
+    /* Driver program to test above function */
+    static public void main(String[] args) {
+        String txt = "San jose down town";
+        String pat = "own";
+        System.out.println(countFreq(pat, txt));
+    }
+}
+
