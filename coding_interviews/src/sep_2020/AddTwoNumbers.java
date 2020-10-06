34e873d8e9783d435c71ca5169efb46eb7fedbab
Ferdis17 <fmuriyesu@gmail.com>
2020-10-05 23:23:59 -0400

    add two numbers in linkedlist

---
+++ coding_interviews/src/sep_2020/AddTwoNumbers.java
@@ -0,0 +1,60 @@
+package sep_2020;
+
+public class AddTwoNumbers {
+
+     static class ListNode {
+
+     int val;
+     ListNode next;
+
+     ListNode(int val) {
+         this.val = val;
+     }
+  }
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    static void printList(ListNode node)
+    {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        printList(addTwoNumbers(l1, l2));
+
+//        System.out.println("Given Linked list");
+//        list.printList(head);
+//        head = list.reverse(head);
+//        System.out.println("");
+//        System.out.println("Reversed linked list ");
+//        list.printList(head);
+    }
+}
