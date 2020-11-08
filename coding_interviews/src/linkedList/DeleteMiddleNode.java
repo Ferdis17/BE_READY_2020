381308b6095201b68a7b6cc00d5e5af9f836dfac
Ferdis17 <fmuriyesu@gmail.com>
2020-11-08 10:43:42 -0500

    partition

--- coding_interviews/src/linkedList/DeleteMiddleNode.java
+++ coding_interviews/src/linkedList/DeleteMiddleNode.java
@@ -1,5 +1,8 @@
 package linkedList;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class DeleteMiddleNode {
 
     Node head;
@@ -33,8 +36,28 @@ public class DeleteMiddleNode {
             head = head.next;
         }
     }
+
+    public static Node partitionALinkedList(Node n, int k) {
+        Node head = n;
+        Node tail = n;
+
+        while (n != null) {
+            Node next = n.next;
+            if (n.value < k) {
+                n.next = head;
+                head = n;
+            } else {
+                tail.next = n;
+                tail = n;
+            }
+            n = next;
+        }
+        tail.next = null;
+        return head;
+    }
     public static void main(String[] args) {
         DeleteMiddleNode add = new DeleteMiddleNode();
+
         add.push(12);
         add.push(13);
         add.push(11);
@@ -43,15 +66,26 @@ public class DeleteMiddleNode {
         add.push(15);
         add.push(10);
 
+        Node start = new Node(12);
+        start.next = new Node(13);
+        start.next.next = new Node(11);
+        start.next.next.next = new Node(23);
+        start.next.next.next.next = new Node(42);
+        start.next.next.next.next.next = new Node(15);
+        start.next.next.next.next.next.next = new Node(10);
+
+        System.out.println();
+        System.out.println(partitionALinkedList(start, 23));
+        displayElements(start);
+
+        System.out.println();
+
         System.out.println("before deleting a node");
         displayElements(add.head);
 
         System.out.println("after deleting a node");
         System.out.println(deleteMiddleNode(add.head.next.next));
-
         displayElements(add.head);
-
-
     }
 
 }
